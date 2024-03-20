10239dbd216f23e34cf9134e6d83ff48a8f232ff
Érica Vieira <ericaj.vieira13@gmail.com>
2024-03-19 21:41:44 -0300

    Adiciona Atividade-1 Variaveis e tipos de dados

---
+++ Atividade1Variaveis/Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Atividade 1 - Variaveis - Tipos de Dados - Calculos - Incremento e Decremento:
+
+       //1:
+//        int  media1, media2, soma;
+//        float mediaTotal;
+//        media1=((8+9+7)/3);
+//        System.out.println("média aritmética dos números 8,9 e 7 e " + media1);
+//        media2=((4+5+6)/3);
+//        System.out.println("média aritmética dos números 8,9 e 7 e " + media2);
+//        soma = media1 + media2;
+//        System.out.println("A soma das duas médias e " + soma);
+//        mediaTotal = ((media1 + media2)/2);
+//        System.out.println("A média das médias " + mediaTotal);
+
+        //2:
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.printf("Digite um numero!");
+//        int numero = scanner.nextInt();
+//        System.out.println("O numero que você digitou eh " + numero);
+//        System.out.println("e o seu antecessor e " + --numero);
+//       ++numero;
+//        System.out.println("e o sucessor eh " + ++numero);
+//
+        //3:
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("digite o primeiro numero!");
+//        int numero1= scanner.nextInt();
+//        System.out.println("Digite o segundo numero!");
+//        int numero2= scanner.nextInt();
+//        int soma = numero1+numero2;
+//        int sub = numero1-numero2;
+//        int mult = numero1*numero2;
+//        System.out.println("A soma doas numeros e " + soma + ", a subtração e "+ sub+", por fim a multiplicação e " + mult);
+
+//        4:
+//            Scanner scaner = new Scanner(System.in);
+//        System.out.println("digite seu nome e sobrenome!");
+//        String nome = scaner.nextLine();
+//        System.out.println("digite seu nome endereço");
+//        String endereco = scaner.nextLine();
+//        System.out.println("digite sua idade!");
+//        int idade = scaner.nextInt();
+//        System.out.println("Seu nome e sobrenome e " + nome + " seu endereco " + endereco + " e sua idade " + idade);
+
+//        5:
+//        Scanner scaner = new Scanner(System.in);
+//        System.out.println("=====Cadastro=====");
+//        System.out.println("digite seu nome e sobrenome!");
+//        String nome = scaner.nextLine();
+//        System.out.println("digite sua idade!");
+//        int idade = scaner.nextInt();
+//        int AnoNascimento = (2024 - idade);
+//        System.out.println("Seu nome e sobrenome e " + nome +  " e sua idade " + idade + " logo, você nasceu no ano " + AnoNascimento);
+
+
+
+    }
+}
